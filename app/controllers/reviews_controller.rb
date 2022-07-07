16524e6d588c9a518c921553a32b967e8fa2b343
haikoonrails <lachinmashayex@gmail.com>
2022-07-07 13:39:55 +0430

    38-Adding new database fields with migrations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -69,7 +69,8 @@ class ReviewsController < ApplicationController
 
   # self create function
   def form_param
-    params.require(:review).permit(:title, :restaurant, :body, :score)
+    params.require(:review).permit(:title, :restaurant, :body, :score,
+    :ambiance)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_193855) do
+ActiveRecord::Schema.define(version: 2022_07_07_084609) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_06_30_193855) do
     t.string "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end

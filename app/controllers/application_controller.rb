dc90f8b5c3519fb58bbbe46d482a9b760b64efed
haikoonrails <lachinmashayex@gmail.com>
2022-08-06 20:53:07 +0430

    75 The application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # before any action in any controller loads (new,update,create,destroy,...)
+  #find the current_user
+  before_action :find_current_user
+
+  def find_current_user
+    @current_user = User.find(session[:user_id])
+  end
 end
